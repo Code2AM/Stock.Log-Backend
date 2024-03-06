f2b9e562a39d006c232629f0eec83af735a2292c
ClownYH <qwer1745@gmail.com>
2024-03-06 14:09:52 +0900

    Remove : 불필요한 Class 제거

--- src/main/java/com/code2am/stocklog/domain/journals/infra/SellServ.java
+++
@@ -1,8 +0,0 @@
-package com.code2am.stocklog.domain.journals.infra;
-
-import com.code2am.stocklog.domain.sell.service.SellService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class SellServ extends SellService {
-}
