11698872434e8accfa4f28fb20f8ed833c41c465
ClownYH <qwer1745@gmail.com>
2024-03-08 19:49:47 +0900

    Fix : 매매일지 삭제 기능 변경, POST 요청으로 변경하였고, 또 다른 원인인 스택 오버 플로 문제가 발생(로직 변경으로 해결)

--- src/main/java/com/code2am/stocklog/domain/journals/controller/JournalsController.java
+++ src/main/java/com/code2am/stocklog/domain/journals/controller/JournalsController.java
@@ -2,6 +2,7 @@ package com.code2am.stocklog.domain.journals.controller;
 
 import com.code2am.stocklog.domain.auth.common.util.AuthUtil;
 import com.code2am.stocklog.domain.journals.models.dto.JournalsDTO;
+import com.code2am.stocklog.domain.journals.models.entity.Journals;
 import com.code2am.stocklog.domain.journals.service.JournalsService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -56,6 +57,28 @@ public class JournalsController {
 
         List<JournalsDTO> result = journalsService.readJournalsByUserId(userId);
         System.out.println(result);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @Operation(
+            summary = "매매일지 삭제",
+            description = "매매일지를 삭제합니다.",
+            tags = {"POST"}
+    )
+    @PostMapping("/delete")
+    public ResponseEntity deleteJournalsByJournalsId(@RequestBody JournalsDTO journals){
+
+        if(Objects.isNull(journals)){
+            System.out.println("요청이 전달 안됨");
+            return ResponseEntity.badRequest().body("값이 없습니다.");
+        }
+
+        System.out.println("삭제를 위한 요청" + journals.getJournalId());
+
+        Integer journalId = journals.getJournalId();
+
+        String result = journalsService.deleteJournalsByJournalsId(journalId);
 
         return ResponseEntity.ok(result);
     }
