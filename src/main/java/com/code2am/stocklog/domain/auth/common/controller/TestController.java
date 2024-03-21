87f43ff3d4531adc0f6b710317c5952a56e847db
ClownYH <qwer1745@gmail.com>
2024-03-21 18:11:35 +0900

    test mapping change

--- src/main/java/com/code2am/stocklog/domain/auth/common/controller/TestController.java
+++ src/main/java/com/code2am/stocklog/domain/auth/common/controller/TestController.java
@@ -4,17 +4,14 @@ package com.code2am.stocklog.domain.auth.common.controller;
 import com.code2am.stocklog.domain.users.models.dto.UserDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
 @RequiredArgsConstructor
 public class TestController {
 
-    @PostMapping("/test")
+    @GetMapping("/test")
     public ResponseEntity<String> test() {
         System.out.println("test/도착");
 
